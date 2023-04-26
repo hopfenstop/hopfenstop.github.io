8a678372a07c0155284945ec7c1ff60d6a3b4ab4
tobirohrer <tobias.rohrer@outlook.com>
2023-04-26 12:13:44 +0200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# HopfenStop
-
-## Run in Development Mode
-
-In order to run HofepnStop on your local machine please run:
-
-1. `npm i` to install the dependencies
-2. `npm run develop` to run the local develoment server
-3. Happy hacking 🤖🤖🤖
---
+++ component---src-pages-about-js-ddf6e427bf0b60c6f9ff.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhopfenstop=self.webpackChunkhopfenstop||[]).push([[682],{5465:function(e,t,n){n.r(t),n.d(t,{Head:function(){return o}});var u=n(7294),l=n(5939);const o=()=>u.createElement("title",null,"About Me");t.default=()=>u.createElement(l.Z,{pageTitle:"About Me"},u.createElement("p",null,"Hi there! I'm the proud creator of this site, which I built with Gatsby."))}}]);
+//# sourceMappingURL=component---src-pages-about-js-ddf6e427bf0b60c6f9ff.js.map
