0dfa1f6d8408029c8c8f542d35539c5694d3cdae
Sebastian Blam <sebastian.blam@senacor.com>
2023-04-26 21:47:31 +0200

    Use same map tile layer in kiosk detail view

--- src/components/kiosk_detail_view/kiosk_map_header.js
+++ src/components/kiosk_detail_view/kiosk_map_header.js
@@ -3,10 +3,11 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet"
 
 const KioskHeaderMap = ({lat, long}) => (
   <MapContainer style={{ height: '300px' }} center={[lat, long]} zoom={16} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
+    <TileLayer
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          ext='png'
+          url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}'
+    />
     <Marker 
         position={[lat, long]}
     >
