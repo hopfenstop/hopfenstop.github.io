938676ec084fc181664ac9ca06d1318edd9ea37e
tobirohrer <tobias.rohrer@outlook.com>
2023-04-27 08:38:23 +0200

    Added comments about the webpack custom config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+// Fixing build errors when building app using webpack. See 
+// https://www.gatsbyjs.com/docs/debugging-html-builds/#fixing-third-party-modules for more information 
+
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     if (stage === "build-html" || stage === "develop-html") {
       actions.setWebpackConfig({
