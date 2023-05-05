7367c17ea2bea854aed59dd6e32279f9ebf4a337
Sebastian Blam <sebastian.blam@senacor.com>
2023-05-05 20:01:04 +0200

    Apply content wrapper to data protection and imprint

--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -1,23 +1,26 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { styles } from '../styles'
 
 const Imprint = () => {
   return (
     <Layout pageTitle="Impressum">
-        <p><b>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</b></p>
-        <p>
-            Blam, Sebastian; Kroth, Kai und Rohrer, Tobias - Sekato GbR<br />
-            Kaufunger Str. 7<br />
-            60486 Frankfurt am Main<br />
-            Hessen<br />
-            Deutschland<br />
-            <br />
-            E-Mail: info@hopfenstop.de<br />
-            Telefon: +49(0)1601845214<br />
-            Internetadresse: hopfenstop.de<br />
-            <br />
-            Umsatzsteuer-Identifikationsnummer: DE312863617<br />
-        </p>
+        <div style={styles.contentWrapper}>
+          <p><b>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</b></p>
+          <p>
+              Blam, Sebastian; Kroth, Kai und Rohrer, Tobias - Sekato GbR<br />
+              Kaufunger Str. 7<br />
+              60486 Frankfurt am Main<br />
+              Hessen<br />
+              Deutschland<br />
+              <br />
+              E-Mail: info@hopfenstop.de<br />
+              Telefon: +49(0)1601845214<br />
+              Internetadresse: hopfenstop.de<br />
+              <br />
+              Umsatzsteuer-Identifikationsnummer: DE312863617<br />
+          </p>
+        </div>
     </Layout>
   )
 }
