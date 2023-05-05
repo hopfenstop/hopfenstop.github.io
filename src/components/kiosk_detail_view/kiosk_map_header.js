c072dd1ff0a329071d0dc3df6c97ddd01d5edb0f
Sebastian Blam <sebastian.blam@senacor.com>
2023-05-05 19:57:44 +0200

    Reduce height on map in kiosk detail view

--- src/components/kiosk_detail_view/kiosk_map_header.js
+++ src/components/kiosk_detail_view/kiosk_map_header.js
@@ -2,7 +2,7 @@ import React from "react"
 import { MapContainer, TileLayer, Marker } from "react-leaflet"
 
 const KioskHeaderMap = ({lat, long}) => (
-  <MapContainer style={{ height: '300px' }} center={[lat, long]} zoom={16} scrollWheelZoom={false}>
+  <MapContainer style={{ height: '250px' }} center={[lat, long]} zoom={16} scrollWheelZoom={false}>
     <TileLayer
           attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           ext='png'
