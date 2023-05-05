6792d54fb3d9152c0e1cfc056e1e08d1ce82b692
Sebastian Blam <sebastian.blam@senacor.com>
2023-05-05 20:11:13 +0200

    Updates

---
+++ component---src-pages-about-js-34062e1a9550afff30ab.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhopfenstop=self.webpackChunkhopfenstop||[]).push([[682],{5465:function(e,t,n){n.r(t),n.d(t,{Head:function(){return o}});var u=n(7294),l=n(6769);const o=()=>u.createElement("title",null,"About Me");t.default=()=>u.createElement(l.Z,{pageTitle:"About Me"},u.createElement("p",null,"Hi there! I'm the proud creator of this site, which I built with Gatsby."))}}]);
+//# sourceMappingURL=component---src-pages-about-js-34062e1a9550afff30ab.js.map
