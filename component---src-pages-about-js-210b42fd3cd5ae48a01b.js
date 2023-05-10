a9c3dac743307b5e5a0e85c294bb508a870a6d02
kkroth <kaikroth@gmail.com>
2023-05-10 09:09:48 +0200

    Updates

--- component---src-pages-about-js-210b42fd3cd5ae48a01b.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhopfenstop=self.webpackChunkhopfenstop||[]).push([[682],{5465:function(e,t,n){n.r(t),n.d(t,{Head:function(){return o}});var u=n(7294),l=n(5476);const o=()=>u.createElement("title",null,"About Me");t.default=()=>u.createElement(l.Z,{pageTitle:"About Me"},u.createElement("p",null,"Hi there! I'm the proud creator of this site, which I built with Gatsby."))}}]);
-//# sourceMappingURL=component---src-pages-about-js-210b42fd3cd5ae48a01b.js.map
